7d42a02647b1613de3f678ddc256b70c4cbdea1b
inni-iii <inhee0@tukorea.ac.kr>
2023-02-21 15:02:40 +0900

    전화번호 목록

---
+++ 23y02m/21th/인희_전화번호_목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    
+    # 오름차순으로 정렬
+    phone_book.sort()
+    
+    for i, j in zip(phone_book, phone_book[1:]):
+        if j.startswith(i):
+            return False
+    return True
