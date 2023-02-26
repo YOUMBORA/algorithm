027df1d29837a80e803d0ac423c9cc5c8977d714
datakim1201 <ssukim1201@gmail.com>
2023-02-26 15:42:12 +0900

    add stock

---
+++ 23y02m/27th/주식가격_한성.py
@@ -0,0 +1,17 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)-1):
+        cnt = 0
+        for j in range(i+1,len(prices)):
+            if prices[i] <= prices[j]:
+                cnt +=1
+            else:
+                cnt +=1
+                break
+        answer.append(cnt)
+    answer.append(0)
+    return answer
+
+if __name__ == '__main__':
+    prices = [1, 2, 3, 2, 3]
+    print(solution(prices))
