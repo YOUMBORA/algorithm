a7b8a92beb0ed6da24fdaad66d38f9f1afb16738
neulvo <kzvxy@naver.com>
2023-02-27 09:39:03 +0900

    성현 2023/02/27

---
+++ 23y02m/27th/성현_다리를 지나는 트럭.py
@@ -0,0 +1,16 @@
+def solution(bridge_length, weight, truck_weights):
+    second = 0
+    completed = []
+    in_bridge = [0]*bridge_length
+    size=  len(truck_weights)
+    while len(completed)<size:
+        second+=1
+        top = in_bridge.pop(0)
+        if top!=0:
+            completed.append(top)
+        if len(truck_weights)>0:
+            if sum(in_bridge) + truck_weights[0] <= weight:
+                in_bridge.append(truck_weights.pop(0))
+            else:
+                in_bridge.append(0)
+    return second
