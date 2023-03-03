530deb945994462f43a49ec8f5de73b8a2e845b2
inni-iii <inhee0@tukorea.ac.kr>
2023-03-03 16:56:42 +0900

    fail

--- 23y03m/3rd/인희_전력망을 둘로 나누기.py
+++ 23y03m/3rd/인희_전력망을 둘로 나누기.py
@@ -2,6 +2,25 @@
 # dfs를 이용하여 각각의 송전탑 개수를 구한 후, 차이를 list에 넣어주기
 # list의 최소값 구하기
 
+# dfs를 잘 못해서 생기는 대참사 발생
+
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end = ' ')
+    
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+            
+
 def solution(n, wires):
     answer = -1
+    
+    for idx in range(len(wires)):
+        wire = wires.copy()
+        del wire[idx]
+
+        visited = [False] * (n+1)
+        dfs(wire, 1, visited)
+    
     return answer
