61f46ff5871d5a772cd4085b102494354bcdd8c7
Dongmin Choi <chrisandjj1218@gmail.com>
2023-03-22 16:05:52 +0900

    Create 동민_가장먼노드.py

---
+++ 23y03m/22th/동민_가장먼노드.py
@@ -0,0 +1,38 @@
+import heapq
+
+def dijkstra(adj_list, start, n):
+    INF = 1e9
+    visited = [False] * n
+    dist = [INF] * n
+    
+    heap_list = []
+    heapq.heappush(heap_list,(0,0))
+    
+    dist[0] = 0
+    
+    for _ in range(n-1):
+        cur_dist, cur_node = heapq.heappop(heap_list)
+        visited.append(cur_node)
+
+        for id in adj_list[cur_node]:
+            if (dist[id] > 1 + cur_dist):
+                dist[id] = 1 + cur_dist
+                heapq.heappush(heap_list, (dist[id],id))
+
+    max_cnt = dist.count(max(dist))
+    return max_cnt
+
+def solution(n, edges):
+    ## 1~n번까지 노드 존재 => 0~n-1로 치환
+    ## may use dijkstra
+
+    graph = [[] for _ in range(n)]
+    
+    for edge in edges:
+        start_node = edge[0]-1
+        end_node = edge[1]-1
+        graph[start_node].append(end_node)
+        graph[end_node].append(start_node)
+    
+    answer = dijkstra(graph, 0, n)
+    return answer
