6257e1bc8874e8861287f59e76926518127fce2f
neulvo <kzvxy@naver.com>
2023-04-17 18:36:14 +0900

    성현 2023/04/17 수정

--- 23y04m/17th/성현_로봇 청소기.py
+++ 23y04m/17th/성현_로봇 청소기.py
@@ -6,10 +6,9 @@ N, M  = map(int,input().split())
 r, c, d = map(int,input().split())
 graph = [list(map(int, input().split())) for _ in range(N)]
 direct = [(-1,0),(0,1),(1,0),(0,-1)]
-visited = [[False]*M for _ in range(N)]
 
-visited[r][c] =1
 answer = 1
+graph[r][c] = 2
 
 while True:
     flag = 0
@@ -18,13 +17,12 @@ while True:
         nr = r + direct[d][0]
         nc = c + direct[d][1]
         if 0<=nr<N and 0<=nc<M and graph[nr][nc]==0:
-            if not visited[nr][nc]:
-                visited[nr][nc] = True
-                answer += 1
-                r = nr
-                c = nc 
-                flag = 1
-                break 
+            graph[nr][nc] =2
+            answer += 1
+            r = nr
+            c = nc 
+            flag = 1
+            break 
     if flag == 0:
         if graph[r-direct[d][0]][c-direct[d][1]] == 1:
             print(answer)
